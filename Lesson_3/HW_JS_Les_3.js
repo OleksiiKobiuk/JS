65304b33a09de5912198cc893446b79452c31dd1
OleksiiKobiuk <al_kob@ukr.net>
2021-04-01 01:07:50 +0300

    HW3

---
+++ Lesson_3/HW_JS_Les_3.js
@@ -0,0 +1,228 @@
+// // 1) Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// //     a) отримує текст з параграфа з id "content"
+//
+// let content = document.getElementById('content');
+// console.log(content);
+//
+// //     b) отримує текст з блоку з id "rules"
+//
+// let rules = document.getElementById('rules');
+// console.log(rules);
+//
+//
+// //     c) замініть текст параграфа з id 'content' на будь-який інший
+//
+// content.innerText = "Hello Victor";
+// console.log(content);
+//
+//
+// //     d) замініть текст параграфа з id 'rules' на будь-який інший
+//
+// rules.innerText = "Hello Lorem";
+// console.log(rules);
+//
+// //     e) змініть кожному елементу колір фону на червоний
+//
+// content.innerHTML = '<p style="background-color: red">"Hello Victor"</p>'
+// rules.innerHTML = '<div style="background-color: red">"Hello Lorem"</div>'
+//
+//
+// //     f) змініть кожному елементу колір тексту на синій
+//
+// content.innerHTML = '<p style="background-color: lightskyblue">"Hello Victor"</p>';
+// rules.innerHTML = '<div style="background-color: lightskyblue">"Hello Lorem"</div>';
+//
+// //     g) отримати весь список класів елемента з id=rules і вивести їх в console.log
+//
+// let arr = document.getElementById('rules');
+// console.log(arr);
+//
+// //     h) отримати всі елементи з класом fc_rules
+//
+// const fc_rules = document.getElementsByClassName('fc_rules')
+//
+// //     i) поміняти колір тексту у всіх елементів fc_rules на червоний
+//
+// for (const fcRule of fc_rules) {
+//     fcRule.style.color = 'red';
+// }
+
+
+let users = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+}];
+//
+// 2) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+//    Вставити цей блок на сторінку
+
+
+//
+// for (const user of users) {
+//     const userDiv = document.createElement('div');
+//     userDiv.innerHTML = `Ім'я: ${user.name} <br> Вік: ${user.age} <br> Статус: ${user.status} <br> Адреса: ${user.address}`;
+//         userDiv.style.color = 'red';
+//         document.body.appendChild(userDiv);
+//     }
+// 3) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+//    Блок з адресою зробити окремим блоком, з блоками для кожної властивості
+//
+
+for (const user of users) {
+    const userDiv = document.createElement('div');
+    userDiv.innerHTML = `Ім'я: ${user.name} <br> Вік: ${user.age} <br> Статус: ${user.status} <br> Адреса:`;
+    userDiv.style.color = 'darkblue';
+    document.body.appendChild(userDiv);
+    for (const address1 in user.address) {
+        const userAddress = document.createElement('div');
+        userAddress.innerHTML = user.address[address1];
+        userDiv.appendChild(userAddress);
+    }
+}
+// ______________________________________________________________________________________________________________________________________________________
+//                                                                 РОБОТА В АУДИТОРІЇ
+// ______________________________________________________________________________________________________________________________________________________
+//
+// Взяти файл template_2.html та працювати в ньому
+// 1) Напишіть код, який :
+//     a) змінює колір тексту елемнту з ід main_header на назву групи (mon-year)
+//     b) робить шириниу елементу ul 400px
+//     c) робить шириниу всіх елементів з класом linkList шириною 50%
+//     d) отримує текст який зберігається в елементі з класом listElement2
+//     e) отримує всі елементи li та змінює ім колір фону на сірий
+//     f) отримує всі елементи 'a' та додає їм клас anchor
+//     g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+//     h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+//     i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
+//     j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
+//     k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
+//     l) отримати елементи p та змінити їм padding на 20px
+//     m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year)
+//
+// 2) Є масив котрий характеризує правила.
+//    Створити скрипт який ітерує цей масив, та робить кожне правило в окремому блоці.
+//    При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+//    Результатом відпрацювання скріпта повинна бути така ж структура яка міститься в файлі rules.html
+//
+// 			let rules = [
+// 				{
+// 					title: 'Первое правило Бойцовского клуба.',
+// 					body: 'Никому не рассказывать о Бойцовском клубе.'
+// 				},
+// 				{
+// 					title: 'Второе правило Бойцовского клуба.',
+// 					body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+// 				},
+// 				{
+// 					title: 'Третье правило Бойцовского клуба.',
+// 					body: 'В схватке участвуют только двое.'
+// 				},
+// 				{
+// 					title: 'Четвертое правило Бойцовского клуба.',
+// 					body: 'Не более одного поединка за один раз.'
+// 				},
+// 				{
+// 					title: 'Пятое правило Бойцовского клуба.',
+// 					body: 'Бойцы сражаются без обуви и голые по пояс.'
+// 				},
+// 				{
+// 					title: 'Шестое правило Бойцовского клуба.',
+// 					body: 'Поединок продолжается столько, сколько потребуется.'
+// 				},
+// 				{
+// 					title: 'Седьмое правило Бойцовского клуба.',
+// 					body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+// 				},
+// 				{
+// 					title: 'Восьмое и последнее правило Бойцовского клуба.',
+// 					body: 'Новичок обязан принять бой.'
+// 				}
+// 			];
+//
+// ===========================================================================
+//
+// - Дано 2 масиви з рівною кількістю об'єктів.
+// Масиви:
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false},
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false}
+// ];
+//
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+//
+// З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+// Записати цей об'єкт в новий масив
+// Example:
+// let usersWithCities = [
+//     {
+//         id: 1, // <===
+//         name: 'vasya',
+//         age: 31,
+//         status: false,
+//         address: {
+//             user_id: 1, // <===
+//             country: 'Ukraine',
+//             city: 'Ternopil'
+//         }
+//     },
+//     // TO BE CONTINUED .....
+// ]
